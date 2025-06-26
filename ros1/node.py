7a36bea9b47d8f7da900b4b81a9e6601812699c2
tongzj <tongzanjia@qq.com>
2025-06-26 22:42:28 +0800

    init

---
+++ ros1/node.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding: utf-8
+import rospy
+import sys
+
+
+class NodeBase:
+
+    def __init__(self, name):
+        rospy.init_node(name, sys.argv)
+        # log methods
+        self.DEBUG = rospy.logdebug
+        self.INFO = rospy.loginfo
+        self.WARN = rospy.logwarn
+        self.ERROR = rospy.logerr
+        self.FATAL = rospy.logfatal
+
+    def __enter__(self):
+        self.INFO(f"Successful initialization.")
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if exc_type: return False
+        del self
+
+
+if __name__ == '__main__':
+    with NodeBase("demo") as node:
+        try:
+            while not rospy.is_shutdown():
+                node.INFO("Node is running...")
+                rospy.sleep(1)
+        except rospy.ROSInterruptException:
+            pass
