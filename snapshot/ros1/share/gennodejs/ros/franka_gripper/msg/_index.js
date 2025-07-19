ec9fc3c88e869bca5d3da86c550bb1c12d6cfb8a
tongzj <tongzanjia@qq.com>
2025-07-19 18:52:49 +0800

    update

---
+++ snapshot/ros1/share/gennodejs/ros/franka_gripper/msg/_index.js
@@ -0,0 +1,64 @@
+
+"use strict";
+
+let GraspResult = require('./GraspResult.js');
+let StopAction = require('./StopAction.js');
+let HomingActionGoal = require('./HomingActionGoal.js');
+let StopFeedback = require('./StopFeedback.js');
+let MoveActionGoal = require('./MoveActionGoal.js');
+let GraspGoal = require('./GraspGoal.js');
+let GraspActionGoal = require('./GraspActionGoal.js');
+let StopActionFeedback = require('./StopActionFeedback.js');
+let StopResult = require('./StopResult.js');
+let HomingResult = require('./HomingResult.js');
+let GraspActionFeedback = require('./GraspActionFeedback.js');
+let HomingFeedback = require('./HomingFeedback.js');
+let GraspActionResult = require('./GraspActionResult.js');
+let HomingActionResult = require('./HomingActionResult.js');
+let StopGoal = require('./StopGoal.js');
+let MoveFeedback = require('./MoveFeedback.js');
+let StopActionResult = require('./StopActionResult.js');
+let HomingAction = require('./HomingAction.js');
+let StopActionGoal = require('./StopActionGoal.js');
+let MoveAction = require('./MoveAction.js');
+let GraspAction = require('./GraspAction.js');
+let GraspFeedback = require('./GraspFeedback.js');
+let MoveActionFeedback = require('./MoveActionFeedback.js');
+let MoveGoal = require('./MoveGoal.js');
+let MoveActionResult = require('./MoveActionResult.js');
+let HomingActionFeedback = require('./HomingActionFeedback.js');
+let HomingGoal = require('./HomingGoal.js');
+let MoveResult = require('./MoveResult.js');
+let GraspEpsilon = require('./GraspEpsilon.js');
+
+module.exports = {
+  GraspResult: GraspResult,
+  StopAction: StopAction,
+  HomingActionGoal: HomingActionGoal,
+  StopFeedback: StopFeedback,
+  MoveActionGoal: MoveActionGoal,
+  GraspGoal: GraspGoal,
+  GraspActionGoal: GraspActionGoal,
+  StopActionFeedback: StopActionFeedback,
+  StopResult: StopResult,
+  HomingResult: HomingResult,
+  GraspActionFeedback: GraspActionFeedback,
+  HomingFeedback: HomingFeedback,
+  GraspActionResult: GraspActionResult,
+  HomingActionResult: HomingActionResult,
+  StopGoal: StopGoal,
+  MoveFeedback: MoveFeedback,
+  StopActionResult: StopActionResult,
+  HomingAction: HomingAction,
+  StopActionGoal: StopActionGoal,
+  MoveAction: MoveAction,
+  GraspAction: GraspAction,
+  GraspFeedback: GraspFeedback,
+  MoveActionFeedback: MoveActionFeedback,
+  MoveGoal: MoveGoal,
+  MoveActionResult: MoveActionResult,
+  HomingActionFeedback: HomingActionFeedback,
+  HomingGoal: HomingGoal,
+  MoveResult: MoveResult,
+  GraspEpsilon: GraspEpsilon,
+};
